55feb462f6df27b6cccb0be30c1daac9fcd72a62
gustavo <gustavo.maia@b2b.com.pt>
2020-11-20 09:42:34 +0000

    NavController and OnClick Listeners

--- app/src/main/java/com/artemissoftware/poseidonnavegation/ChooseRecipientFragment.kt
+++ app/src/main/java/com/artemissoftware/poseidonnavegation/ChooseRecipientFragment.kt
@@ -5,9 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_choose_recipient.*
+import kotlinx.android.synthetic.main.fragment_main.*
 
 
-class ChooseRecipientFragment : Fragment() {
+class ChooseRecipientFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController : NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,4 +23,20 @@ class ChooseRecipientFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_choose_recipient, container, false)
     }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+        next_btn.setOnClickListener(this)
+        cancel_btn.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.next_btn -> navController.navigate(R.id.action_chooseRecipientFragment_to_specifyAmountFragment)
+            R.id.cancel_btn -> activity!!.onBackPressed()
+        }
+    }
 }
--- app/src/main/java/com/artemissoftware/poseidonnavegation/MainFragment.kt
+++ app/src/main/java/com/artemissoftware/poseidonnavegation/MainFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_main.*
 
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController : NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,5 +21,24 @@ class MainFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+        view_transactions_btn.setOnClickListener(this)
+        send_money_btn.setOnClickListener(this)
+        view_balance_btn.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.view_transactions_btn -> navController.navigate(R.id.action_mainFragment_to_viewTransactionFragment)
+            R.id.send_money_btn -> navController.navigate(R.id.action_mainFragment_to_chooseRecipientFragment)
+            R.id.view_balance_btn -> navController.navigate(R.id.action_mainFragment_to_viewBalanceFragment)
+        }
+    }
+
 
 }
